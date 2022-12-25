a0d9bb6d83cadcc7c790f865df31fe3d8c830e99
Jeffrey Brian Jonelas <107722564+Jepwyy@users.noreply.github.com>
2022-12-25 21:52:32 +0800

    first commit

---
+++ Number Checker/Number Checker/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim number As Integer
+        Try
+            number = Convert.ToInt32(TextBox1.Text)
+            If number >= 0 Then
+                TextBox2.Text = "Positive Number!"
+            Else
+                TextBox2.Text = "Negative Number!"
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Please input a number.", "Invalid Input",
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End Try
+    End Sub
+End Class
